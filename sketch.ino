f72cd5e0b56648ba10d34e0325510d64a18a3917
Dmitri <56200546+Syritx@users.noreply.github.com>
2021-02-13 10:39:18 -0500

    added receive message to the client

--- sketch.ino
+++ sketch.ino
@@ -3,37 +3,59 @@
 WiFiClient client = WiFiClient();
 
 const int BUTTON_PIN = 13;
+const int LED_PIN = 2;
+
 bool IS_DOWN = false;
 bool IS_CONNECTED = false;
 
 void setup() {
     Serial.begin(115200);
 
-    pinMode(BUTTON_PIN, INPUT_PULLUP);
+    pinMode(LED_PIN, OUTPUT);
+    pinMode(BUTTON_PIN, INPUT_PULLUP); 
 
     WiFi.begin((char*)"SSID", (char*)"PASSWORD");
     while (WiFi.status() != WL_CONNECTED) {
-      delay(500);
-      Serial.print("...\n");
+        delay(500);
+        Serial.print("...\n");
     }
 }
 
 void loop() {
     int BUTTON_STATE = digitalRead(BUTTON_PIN);
+    
     if (BUTTON_STATE == HIGH && IS_DOWN == false) {
         IS_DOWN = true;
-        if (!IS_CONNECTED) {
-            client.connect((char*)"HOST", (uint16_t)6060);
-            client.print("Connection from ESP8266");
-            IS_CONNECTED = true;
+
+        if (!client.connected()) {
+            client.connect((char*)"IP", (uint16_t)6060);
+            client.print("hello from esp 8266");
         }
         else {
-            client.print("disconnect_ed");
+            client.print("disconnected");
             client.stop();
-            IS_CONNECTED = false;
         }
     }
     if (BUTTON_STATE == LOW && IS_DOWN == true) {
         IS_DOWN = false;
+    }
+
+
+    if (client.connected()) {
+        char buffer[1024];
+        int id = 0;
+      
+        if (client.available() > 0) {
+            char c = client.read();
+            buffer[id] = c;
+            id++;
+        }
+
+        // the led is wired backwards so the following code means 'on'
+        digitalWrite(LED_PIN, LOW);
+    }
+    else {
+        // the led is wired backwards so the following code means 'off'
+        digitalWrite(LED_PIN, HIGH);
     }
 }
